fcb9e0c21b58fc4a31082f223a43b72baf102f79
Martin Karer <sezz@sezz.at>
2021-12-15 20:07:13 +0100

    added action lookup

---
+++ SezzUI/Core/Helpers/SpellHelper.cs
@@ -0,0 +1,23 @@
+﻿using Lumina.Excel;
+using LuminaAction = Lumina.Excel.GeneratedSheets.Action;
+
+namespace SezzUI.Helpers
+{
+	public static class SpellHelper
+	{
+		public static LuminaAction? GetAction(uint actionId)
+		{
+			ExcelSheet<LuminaAction>? sheet = Plugin.DataManager.GetExcelSheet<LuminaAction>();
+			if (sheet is not null)
+			{
+				LuminaAction? action = sheet.GetRow(actionId);
+				if (action is not null)
+				{
+					return action;
+				}
+			}
+
+			return null;
+		}
+	}
+}
