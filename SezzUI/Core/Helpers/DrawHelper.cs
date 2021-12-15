e712d09533a2a85d4b0eaf867abde948fd6ba4ed
Martin Karer <sezz@sezz.at>
2021-12-15 20:07:28 +0100

    added backdrop drawing

--- SezzUI/Core/Helpers/DrawHelper.cs
+++ SezzUI/Core/Helpers/DrawHelper.cs
@@ -8,15 +8,23 @@ namespace SezzUI.Helpers
 {
 	public static class DrawHelper
 	{
-		public static void DrawPlaceholder(string text, Vector2 pos, Vector2 size, ImDrawListPtr drawList, float opacity = 1)
+        public static void DrawBackdrop(Vector2 pos, Vector2 size, ImDrawListPtr drawList, float opacity = 1)
 		{
             // Background
             drawList.AddRectFilled(pos, pos + size, ImGui.ColorConvertFloat4ToU32(new Vector4(0, 0, 0, 0.5f * opacity)), 0);
 
             // Border
             uint colorBorder = ImGui.ColorConvertFloat4ToU32(new Vector4(1, 1, 1, 0.3f * opacity));
+            drawList.AddRect(pos, pos + size, colorBorder, 0, ImDrawFlags.None, 1);
+        }
+
+        public static void DrawPlaceholder(string text, Vector2 pos, Vector2 size, ImDrawListPtr drawList, float opacity = 1)
+		{
+            // Backdrop
+            DrawBackdrop(pos, size, drawList, opacity);
+
+            // Cross
             uint colorLines = ImGui.ColorConvertFloat4ToU32(new Vector4(1, 1, 1, 0.1f * opacity));
-            drawList.AddRect(pos, pos + size, colorBorder, 0, ImDrawFlags.None, 1); 
             drawList.AddLine(new Vector2(pos.X + 1, pos.Y + 1), new Vector2(pos.X + size.X - 1, pos.Y + size.Y - 2), colorLines, 1); // Top Left -> Bottom Right
             drawList.AddLine(new Vector2(pos.X + 1, pos.Y + size.Y - 2), new Vector2(pos.X + size.X - 1, pos.Y + 1), colorLines, 1); // Bottom Left -> Top Right
 
