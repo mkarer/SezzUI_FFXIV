a644355d0bc9cc3be5181399cf128d18aff88cb7
Martin Karer <sezz@sezz.at>
2021-12-16 03:13:02 +0100

    added status lookup by action

--- SezzUI/Core/Helpers/SpellHelper.cs
+++ SezzUI/Core/Helpers/SpellHelper.cs
@@ -1,5 +1,7 @@
-﻿using Lumina.Excel;
+﻿using System.Linq;
+using Lumina.Excel;
 using LuminaAction = Lumina.Excel.GeneratedSheets.Action;
+using LuminaStatus = Lumina.Excel.GeneratedSheets.Status;
 
 namespace SezzUI.Helpers
 {
@@ -7,6 +9,7 @@ namespace SezzUI.Helpers
 	{
 		public static LuminaAction? GetAction(uint actionId)
 		{
+			// TODO: Level filter?
 			ExcelSheet<LuminaAction>? sheet = Plugin.DataManager.GetExcelSheet<LuminaAction>();
 			if (sheet is not null)
 			{
@@ -14,6 +17,27 @@ namespace SezzUI.Helpers
 				if (action is not null)
 				{
 					return action;
+				}
+			}
+
+			return null;
+		}
+
+		public static LuminaStatus? GetStatusByAction(uint actionId)
+		{
+			LuminaAction? action = GetAction(actionId);
+			if (action is not null)
+			{
+				ExcelSheet<LuminaStatus>? sheet = Plugin.DataManager.GetExcelSheet<LuminaStatus>();
+
+				if (sheet is not null)
+				{
+					string actionName = action.Name.ToString().ToLower();
+					LuminaStatus? status = sheet.FirstOrDefault(status => status.Name.ToString().ToLower().Equals(actionName));
+					if (status is not null)
+					{
+						return status;
+					}
 				}
 			}
 
