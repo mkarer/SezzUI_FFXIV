25d843ffb21b4d73cefbde9cbad1ddef3ba4f63e
Martin Karer <sezz@sezz.at>
2021-12-16 14:12:19 +0100

    added translation animation

--- SezzUI/Modules/JobHud/JobHud.cs
+++ SezzUI/Modules/JobHud/JobHud.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using ImGuiNET;
 using Dalamud.Logging;
 using System.Numerics;
 using System.Collections.Generic;
-using Dalamud.Game;
 using Dalamud.Game.ClientState.Objects.SubKinds;
 
 namespace SezzUI.Modules.JobHud
@@ -16,21 +14,26 @@ namespace SezzUI.Modules.JobHud
         private Animator.Animator _animator = new();
         public bool IsShown { get { return _isShown; } }
         internal bool _isShown = false;
-        private Vector2 _positionOffset = new Vector2(0, 148);
+        private Vector2 _positionOffset = new Vector2(0, 150);
 
         private static readonly Lazy<JobHud> ev = new Lazy<JobHud>(() => new JobHud());
         public static JobHud Instance { get { return ev.Value; } }
         public static bool Initialized { get { return ev.IsValueCreated; } }
 
+        internal Vector4 AccentColor;
         private List<Bar> _bars = new();
         private List<AuraAlert> _auraAlerts = new();
 
         public JobHud()
 		{
             _animator.Timelines.OnShow.Data.DefaultOpacity = 0;
-            _animator.Timelines.OnShow.Chain(new Animator.FadeAnimation(0, 1, 150));
+            _animator.Timelines.OnShow.Data.DefaultOffset.Y = -20;
+            _animator.Timelines.OnShow.Add(new Animator.FadeAnimation(0, 1, 150));
+            _animator.Timelines.OnShow.Add(new Animator.TranslationAnimation(_animator.Timelines.OnShow.Data.DefaultOffset, Vector2.Zero, 150));
+
             _animator.Timelines.OnHide.Data.DefaultOpacity = 1;
-            _animator.Timelines.OnHide.Chain(new Animator.FadeAnimation(1, 0, 500));
+            _animator.Timelines.OnHide.Add(new Animator.FadeAnimation(1, 0, 150));
+            _animator.Timelines.OnHide.Add(new Animator.TranslationAnimation(Vector2.Zero, new Vector2(0, 20), 150));
         }
 
         public override void Enable()
@@ -98,6 +101,9 @@ namespace SezzUI.Modules.JobHud
             PlayerCharacter? player = Service.ClientState.LocalPlayer;
             uint jobId = (player != null ? player.ClassJob.Id : 0);
 
+            if (!Defaults.JobColors.TryGetValue(jobId, out AccentColor))
+                AccentColor = Defaults.IconBarColor;
+
             switch (jobId)
 			{
                 case DelvUI.Helpers.JobIDs.GNB:
@@ -107,17 +113,18 @@ namespace SezzUI.Modules.JobHud
                         bar.Add(new Icon { TextureActionId = 16138, CooldownActionId = 16138, StatusId = 1831, MaxStatusDuration = 20, StatusTarget = Enums.Unit.Player, Level = 2 }); // No Mercy
                         bar.Add(new Icon { TextureActionId = 16154, CooldownActionId = 16154, Level = 56 }); // Rough Divide
                         bar.Add(new Icon { TextureActionId = 16151, CooldownActionId = 16151, StatusId = 1835, MaxStatusDuration = 18, StatusTarget = Enums.Unit.Player, Level = 45 }); // Aurora
-                        bar.Add(new Icon { TextureActionId = 16161, CooldownActionId = 16161, StatusId = 1840, MaxStatusDuration = 7, StatusTarget = Enums.Unit.Player, Level = 68 }); // Heart of Stone
                         bar.Add(new Icon { TextureActionId = 16160, CooldownActionId = 16160, StatusId = 1839, MaxStatusDuration = 15, StatusTarget = Enums.Unit.Player, Level = 64 }); // Heart of Light
+                        bar.Add(new Icon { TextureActionId = 7548, CooldownActionId = 7548, StatusId = 1209, MaxStatusDuration = 6, StatusTarget = Enums.Unit.Player, Level = 32 }); // Arm's Length
                         if (bar.HasIcons) _bars.Add(bar);
                     }
 
-                    // Big Defensives
+                    // Defensives
                     using (Bar bar = new())
                     {
                         bar.Add(new Icon { TextureActionId = 7531, CooldownActionId = 7531, StatusId = 1191, MaxStatusDuration = 20, StatusTarget = Enums.Unit.Player, Level = 8 }); // Rampart
                         bar.Add(new Icon { TextureActionId = 16140, CooldownActionId = 16140, StatusId = 1832, MaxStatusDuration = 20, StatusTarget = Enums.Unit.Player, Level = 6 }); // Camouflage
                         bar.Add(new Icon { TextureActionId = 16148, CooldownActionId = 16148, StatusId = 1834, MaxStatusDuration = 15, StatusTarget = Enums.Unit.Player, Level = 38 }); // Nebula
+                        bar.Add(new Icon { TextureActionId = 16161, CooldownActionId = 16161, StatusId = 1840, MaxStatusDuration = 7, StatusTarget = Enums.Unit.Player, Level = 68 }); // Heart of Stone
                         bar.Add(new Icon { TextureActionId = 16152, CooldownActionId = 16152, StatusId = 1836, MaxStatusDuration = 10, StatusTarget = Enums.Unit.Player, Level = 50 }); // Superbolide
                         if (bar.HasIcons) _bars.Add(bar);
                     }
@@ -161,18 +168,9 @@ namespace SezzUI.Modules.JobHud
             {
                 _animator.Update();
 
-                // Debug anchor (for testing animations)
-                //Vector2 elementSize = new(100, 32);
-                //Vector2 elementPosition = DelvUI.Helpers.Utils.GetAnchoredPosition(origin, elementSize, DelvUI.Enums.DrawAnchor.Center);
-
-                //DelvUI.Helpers.DrawHelper.DrawInWindow("SezzUI_JobHud", elementPosition, elementSize, false, false, (drawList) =>
-                //{
-                //    Helpers.DrawHelper.DrawPlaceholder(Name, elementPosition, elementSize, drawList, _animator.Data.Opacity);
-                //});
-
                 for (int i = 0; i < _bars.Count; i++)
 				{
-                    Vector2 pos = origin + _positionOffset;
+                    Vector2 pos = origin + _positionOffset + _animator.Data.Offset;
                     pos.Y += i * (_bars[i].IconSize.Y + (float)_bars[i].IconPadding);
                     _bars[i].Draw(pos, _animator);
                 }
