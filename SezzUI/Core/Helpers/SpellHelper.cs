186a746a31f1e8c63cea30ce5e4602eb209ded8b
Martin Karer <sezz@sezz.at>
2021-12-16 14:13:06 +0100

    added cooldown check (needs testing)

--- SezzUI/Core/Helpers/SpellHelper.cs
+++ SezzUI/Core/Helpers/SpellHelper.cs
@@ -1,10 +1,23 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Lumina.Excel;
 using LuminaAction = Lumina.Excel.GeneratedSheets.Action;
 using LuminaStatus = Lumina.Excel.GeneratedSheets.Status;
+using Dalamud.Game.ClientState.Objects.SubKinds;
 
 namespace SezzUI.Helpers
 {
+	public class CooldownData
+	{
+		public int ChargesMax = 0;
+		public int ChargesCurrent = 0;
+		public float CooldownPerCharge = 0;
+		public float CooldownTotal = 0;
+		public float CooldownTotalElapsed = 0;
+		public float CooldownRemaining = 0;
+		public float CooldownTotalRemaining = 0;
+	}
+
 	public static class SpellHelper
 	{
 		public static LuminaAction? GetAction(uint actionId)
@@ -42,6 +55,32 @@ namespace SezzUI.Helpers
 			}
 
 			return null;
+		}
+
+		public unsafe static CooldownData GetCooldownData(uint actionId)
+		{
+			CooldownData data = new();
+
+			PlayerCharacter? player = Service.ClientState.LocalPlayer;
+			if (player != null)
+			{
+				uint actionIdAdjusted = DelvUI.Helpers.SpellHelper.Instance.GetSpellActionId(actionId);
+
+				data.ChargesMax = DelvUI.Helpers.SpellHelper.Instance.GetMaxCharges(actionIdAdjusted, player.Level);
+				data.ChargesCurrent = DelvUI.Helpers.SpellHelper.Instance.GetStackCount(data.ChargesMax, actionIdAdjusted);
+				data.CooldownTotal = DelvUI.Helpers.SpellHelper.Instance.GetRecastTime(actionIdAdjusted);
+				data.CooldownTotalElapsed = DelvUI.Helpers.SpellHelper.Instance.GetRecastTimeElapsed(actionIdAdjusted);
+				data.CooldownPerCharge = data.CooldownTotal / data.ChargesMax;
+
+				// TODO: Get correct data by level...
+				float remaining = (data.CooldownTotal - data.CooldownTotalElapsed) % data.CooldownPerCharge;
+				if (remaining > 0)
+				{
+					data.CooldownRemaining = remaining;
+				}
+			}
+
+			return data;
 		}
 	}
 }
