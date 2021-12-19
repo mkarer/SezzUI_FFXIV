711c68c6ba69be8dbeb4df0b7f945066b8a4fe9a
Martin Karer <sezz@sezz.at>
2021-12-19 18:06:26 +0100

    updated actions

--- SezzUI/Modules/JobHud/Jobs/AST.cs
+++ SezzUI/Modules/JobHud/Jobs/AST.cs
@@ -6,6 +6,12 @@ namespace SezzUI.Modules.JobHud.Jobs
         
         public override void Configure(JobHud hud)
         {
+            using (Bar bar = new())
+            {
+                bar.Add(new Icon(bar) { TextureActionId = 3599, StatusActionId = 838, MaxStatusDuration = 30, StatusTarget = Enums.Unit.Target, Level = 4 }); // Combust
+                hud.AddBar(bar);
+            }
+
             base.Configure(hud);
         }
     }
