1c027166626ff41408a84de12f6a04aa4c3b5e60
Martin Karer <sezz@sezz.at>
2021-12-20 23:23:00 +0100

    updated actions

--- SezzUI/Modules/JobHud/Jobs/RPR.cs
+++ SezzUI/Modules/JobHud/Jobs/RPR.cs
@@ -10,6 +10,7 @@ namespace SezzUI.Modules.JobHud.Jobs
         {
             using (Bar bar = new())
             {
+                bar.Add(new Icon(bar) { TextureStatusId = 2586, StatusId = 2586, MaxStatusDuration = 60, StatusTarget = Enums.Unit.Target, Level = 10 }); // Death's Design
                 bar.Add(new Icon(bar) { TextureActionId = 24393, CooldownActionId = 24393, Level = 76 }); // Gluttony
                 bar.Add(new Icon(bar) { TextureActionId = 24405, CooldownActionId = 24405, StatusId = 2599, MaxStatusDuration = 20, StatusTarget = Enums.Unit.Player, Level = 72 }); // Arcane Circle
                 hud.AddBar(bar);
