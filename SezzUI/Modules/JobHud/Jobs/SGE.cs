540bed2ce5d4205dc46ef75622e7044b0fad33e0
Martin Karer <sezz@sezz.at>
2021-12-27 06:23:46 +0100

    show kardia alert

--- SezzUI/Modules/JobHud/Jobs/SGE.cs
+++ SezzUI/Modules/JobHud/Jobs/SGE.cs
@@ -1,4 +1,6 @@
-﻿namespace SezzUI.Modules.JobHud.Jobs
+﻿using System.Numerics;
+
+namespace SezzUI.Modules.JobHud.Jobs
 {
     public sealed class SGE : BasePreset
     {
@@ -6,6 +8,19 @@
         
         public override void Configure(JobHud hud)
         {
+            // Kardia
+            using (AuraAlert aa = new())
+            {
+                aa.StatusId = 2604;
+                aa.InvertCheck = true;
+                aa.Size = new Vector2(48, 48);
+                aa.Position = new Vector2(0, -80);
+                aa.Level = 4;
+                aa.BorderSize = 1;
+                aa.UseActionIcon(24285);
+                hud.AddAlert(aa);
+            };
+
             base.Configure(hud);
         }
     }
