46e64b907caf2bab534067dd6645c7242a7eac24
Martin Karer <sezz@sezz.at>
2021-12-27 06:19:57 +0100

    added optional border and support for status+action icons

--- SezzUI/Modules/JobHud/AuraAlert.cs
+++ SezzUI/Modules/JobHud/AuraAlert.cs
@@ -5,17 +5,19 @@ using SezzUI.Core;
 using System.Numerics;
 using ImGuiNET;
 using ImGuiScene;
+using LuminaStatus = Lumina.Excel.GeneratedSheets.Status;
+using LuminaAction = Lumina.Excel.GeneratedSheets.Action;
 
 namespace SezzUI.Modules.JobHud
 {
-	public sealed class AuraAlert : AnimatedHudElement
-	{
-		public uint? StatusId;
-		public uint[]? StatusIds;
-		public float? MaxDuration;
-		public byte? MinimumStacks;
-		public byte? ExactStacks;
-		public Enums.Unit StatusTarget = Enums.Unit.Player;
+    public sealed class AuraAlert : AnimatedHudElement
+    {
+        public uint? StatusId;
+        public uint[]? StatusIds;
+        public float? MaxDuration;
+        public byte? MinimumStacks;
+        public byte? ExactStacks;
+        public Enums.Unit StatusTarget = Enums.Unit.Player;
 
         public Helpers.JobsHelper.PowerType? PowerType;
         public int? MinimumPowerAmount;
@@ -24,20 +26,20 @@ namespace SezzUI.Modules.JobHud
         public Func<bool>? CustomCondition;
 
         public bool InvertCheck = false;
-		public bool EnableInCombat = true;
-		public bool EnableOutOfCombat = true;
-		public bool TreatWeaponOutAsCombat = true;
+        public bool EnableInCombat = true;
+        public bool EnableOutOfCombat = true;
+        public bool TreatWeaponOutAsCombat = true;
 
-		public Vector2 Position = Vector2.Zero;
-		public Vector2 Size = Vector2.Zero;
+        public Vector2 Position = Vector2.Zero;
+        public Vector2 Size = Vector2.Zero;
 
-		public string? Image
-		{
-			get { return _imagePath; }
-			set
-			{
-				_imagePath = value;
-				if (value != null)
+        public string? Image
+        {
+            get { return _imagePath; }
+            set
+            {
+                _imagePath = value;
+                if (value != null)
                 {
                     _texture = Helpers.ImageCache.Instance.GetImageFromPath(value);
                 }
@@ -46,77 +48,146 @@ namespace SezzUI.Modules.JobHud
                     _texture = null;
                 }
             }
-		}
+        }
 
-		private string? _imagePath;
-		private TextureWrap? _texture;
-        public bool FlipImageHorizontally = false;
-        public bool FlipImageVertically = false;
+        private string? _imagePath;
+        private TextureWrap? _texture;
+        public byte BorderSize = 0;
+
+        public Vector2 ImageUV0 = Vector2.Zero;
+        public Vector2 ImageUV1 = Vector2.One;
+        public bool FlipImageHorizontally
+        {
+            get { return _flipHorizontally; }
+            set
+            {
+                if (value && !_flipHorizontally)
+                {
+                    ImageUV0.X += 1;
+                    ImageUV1.X -= 1;
+                }
+                else if (!value && _flipHorizontally)
+                {
+                    ImageUV0.X -= 1;
+                    ImageUV1.X += 1;
+                }
+            }
+        }
+        private bool _flipHorizontally = false;
+        public bool FlipImageVertically
+        {
+            get { return _flipVertically; }
+            set
+            {
+                if (value && !_flipVertically)
+                {
+                    ImageUV0.Y += 1;
+                    ImageUV1.Y -= 1;
+                }
+                else if (!value && _flipVertically)
+                {
+                    ImageUV0.Y -= 1;
+                    ImageUV1.Y += 1;
+                }
+            }
+        }
+        private bool _flipVertically = false;
 
         /// <summary>
         /// Required job level to enable alert.
         /// </summary>
         public byte Level = 1;
 
-		public AuraAlert()
-		{
-			Animator.Timelines.OnShow.Data.DefaultOpacity = 0;
-			Animator.Timelines.OnShow.Data.DefaultScale = 1.5f;
-			Animator.Timelines.OnShow.Add(new Animator.FadeAnimation(0, 0.8f, 250));
-			Animator.Timelines.OnShow.Add(new Animator.ScaleAnimation(1.5f, 1, 250));
-			Animator.Timelines.Loop.Chain(new Animator.FadeAnimation(0.8f, 0.6f, 250));
-			Animator.Timelines.Loop.Chain(new Animator.FadeAnimation(0.6f, 0.8f, 250));
-			Animator.Timelines.OnHide.Data.DefaultOpacity = 1;
-			Animator.Timelines.OnHide.Add(new Animator.FadeAnimation(0.8f, 0, 250));
-			Animator.Timelines.OnHide.Add(new Animator.ScaleAnimation(1, 1.5f, 250));
-		}
+        public AuraAlert()
+        {
+            Animator.Timelines.OnShow.Data.DefaultOpacity = 0;
+            Animator.Timelines.OnShow.Data.DefaultScale = 1.5f;
+            Animator.Timelines.OnShow.Add(new Animator.FadeAnimation(0, 0.8f, 250));
+            Animator.Timelines.OnShow.Add(new Animator.ScaleAnimation(1.5f, 1, 250));
+            Animator.Timelines.Loop.Chain(new Animator.FadeAnimation(0.8f, 0.6f, 250));
+            Animator.Timelines.Loop.Chain(new Animator.FadeAnimation(0.6f, 0.8f, 250));
+            Animator.Timelines.OnHide.Data.DefaultOpacity = 1;
+            Animator.Timelines.OnHide.Add(new Animator.FadeAnimation(0.8f, 0, 250));
+            Animator.Timelines.OnHide.Add(new Animator.ScaleAnimation(1, 1.5f, 250));
+        }
 
-		public override void Draw(Vector2 origin, int elapsed = 0)
-		{
+        public bool UseStatusIcon(uint statusId)
+        {
+            LuminaStatus? status = Helpers.SpellHelper.GetStatus(statusId);
+            if (status != null)
+            {
+                _texture = DelvUI.Helpers.TexturesCache.Instance.GetTextureFromIconId(status.Icon);
+                if (_texture != null)
+                {
+                    (ImageUV0, ImageUV1) = Helpers.DrawHelper.GetTexCoordinates(Size, 0f, true); // TODO
+                }
+            }
+
+            return false;
+        }
+
+        public bool UseActionIcon(uint actionId)
+        {
+            uint actionIdAdjusted = DelvUI.Helpers.SpellHelper.Instance.GetSpellActionId(actionId);
+            LuminaAction? action = Helpers.SpellHelper.GetAction(actionIdAdjusted);
+            if (action != null)
+            {
+                _texture = DelvUI.Helpers.TexturesCache.Instance.GetTextureFromIconId(action.Icon);
+                if (_texture != null)
+                {
+                    (ImageUV0, ImageUV1) = Helpers.DrawHelper.GetTexCoordinates(Size);
+                }
+            }
+
+            return false;
+        }
+
+        public override void Draw(Vector2 origin, int elapsed = 0)
+        {
             if (StatusId == null && StatusIds == null && PowerType == null && CustomCondition == null) { return; }
 
-			PlayerCharacter? player = Plugin.ClientState.LocalPlayer;
-			Status? status = null;
-			bool conditionsFailed = false;
-			if (player == null)
-			{
-				conditionsFailed = true;
-				Hide(elapsed > 2000);
-			}
-			else
-			{
-				bool inCombat = EventManager.Combat.IsInCombat(TreatWeaponOutAsCombat);
-				if ((inCombat && !EnableInCombat) || (!inCombat && !EnableOutOfCombat))
-				{
-					conditionsFailed = true;
-					Hide(elapsed > 2000);
-				}
+            PlayerCharacter? player = Plugin.ClientState.LocalPlayer;
+            Status? status = null;
+            bool conditionsFailed = false;
+            if (player == null)
+            {
+                conditionsFailed = true;
+                Hide(elapsed > 2000);
+            }
+            else
+            {
+                bool inCombat = EventManager.Combat.IsInCombat(TreatWeaponOutAsCombat);
+                if ((inCombat && !EnableInCombat) || (!inCombat && !EnableOutOfCombat))
+                {
+                    conditionsFailed = true;
+                    Hide(elapsed > 2000);
+                }
 
-				if (!conditionsFailed)
-				{
+                if (!conditionsFailed)
+                {
                     // Status Condition
                     if (StatusId != null || StatusIds != null)
                     {
-					    if (StatusId != null)
-					    {
-						    status = Helpers.SpellHelper.GetStatus((uint)StatusId, StatusTarget);
-					    }
-					    else if (StatusIds != null)
-					    {
-						    status = Helpers.SpellHelper.GetStatus(StatusIds, StatusTarget);
+                        if (StatusId != null)
+                        {
+                            status = Helpers.SpellHelper.GetStatus((uint)StatusId, StatusTarget);
+                        }
+                        else if (StatusIds != null)
+                        {
+                            status = Helpers.SpellHelper.GetStatus(StatusIds, StatusTarget);
 
-					    }
+                        }
 
-					    if ((!InvertCheck && status == null) || (InvertCheck && status != null))
-					    {
-						    conditionsFailed = true;
-						    Hide(elapsed > 2000);
-					    }
-					    if (!conditionsFailed && status != null && ((ExactStacks != null && status.StackCount != ExactStacks) || (MinimumStacks != null && status.StackCount < MinimumStacks)))
-					    {
-						    conditionsFailed = true;
-						    Hide(elapsed > 2000);
-					    }
+                        if ((!InvertCheck && status == null) || (InvertCheck && status != null))
+                        {
+                            conditionsFailed = true;
+                            Hide(elapsed > 2000);
+                        }
+                        if (!conditionsFailed && status != null && ((ExactStacks != null && status.StackCount != ExactStacks) || (MinimumStacks != null && status.StackCount < MinimumStacks)))
+                        {
+                            conditionsFailed = true;
+                            Hide(elapsed > 2000);
+                        }
                     }
 
                     // Power Condition
@@ -142,78 +213,72 @@ namespace SezzUI.Modules.JobHud
                     }
                 }
 
-				if (!conditionsFailed)
-				{
-					Show();
-				}
-			}
+                if (!conditionsFailed)
+                {
+                    Show();
+                }
+            }
 
- 			if (IsShown || Animator.IsAnimating)
-			{
-				Animator.Update();
-	
-				// Draw aura alert
-				Vector2 elementSize = Size * Animator.Data.Scale;
-				Vector2 elementPosition = DelvUI.Helpers.Utils.GetAnchoredPosition(origin, elementSize, Enums.DrawAnchor.Center);
-				elementPosition.X += Position.X + Animator.Data.Offset.X;
-				elementPosition.Y += Position.Y + Animator.Data.Offset.Y;
+            if (IsShown || Animator.IsAnimating)
+            {
+                Animator.Update();
+
+                // Draw aura alert
+                Vector2 elementSize = Size * Animator.Data.Scale;
+                Vector2 elementPosition = DelvUI.Helpers.Utils.GetAnchoredPosition(origin, elementSize, Enums.DrawAnchor.Center);
+                elementPosition.X += Position.X + Animator.Data.Offset.X;
+                elementPosition.Y += Position.Y + Animator.Data.Offset.Y;
 
                 string windowId = "SezzUI_AuraAlert";
-				DelvUI.Helpers.DrawHelper.DrawInWindow(windowId, elementPosition, elementSize, false, false, (drawList) =>
-				{
-					if (_texture != null)
-					{
+                DelvUI.Helpers.DrawHelper.DrawInWindow(windowId, elementPosition, elementSize, false, false, (drawList) =>
+                {
+                    if (_texture != null)
+                    {
                         // Texture
-                        Vector2 uv0 = Vector2.Zero;
-                        Vector2 uv1 = Vector2.One;
+                        drawList.AddImage(_texture.ImGuiHandle, elementPosition, elementPosition + elementSize, ImageUV0, ImageUV1, ImGui.ColorConvertFloat4ToU32(new Vector4(1, 1, 1, Animator.Data.Opacity)));
 
-                        if (FlipImageHorizontally)
+                        // Border
+                        if (BorderSize > 0)
                         {
-                            uv0.X = 1;
-                            uv1.X = 0;
+                            drawList.AddRect(elementPosition, elementPosition + elementSize, ImGui.ColorConvertFloat4ToU32(new Vector4(1, 1, 1, 0.3f * Animator.Data.Opacity)), 0, ImDrawFlags.None, BorderSize);
+                        }
+                    }
+                    else
+                    {
+                        // Failover Text
+                        drawList.AddRectFilled(elementPosition, elementPosition + elementSize, ImGui.ColorConvertFloat4ToU32(new Vector4(0, 0, 0, 0.5f * Animator.Data.Opacity)), 0);
+                        drawList.AddRect(elementPosition, elementPosition + elementSize, ImGui.ColorConvertFloat4ToU32(new Vector4(1, 1, 1, 0.3f * Animator.Data.Opacity)), 0, ImDrawFlags.None, 1);
+
+                        bool fontPushed = DelvUI.Helpers.FontsManager.Instance.PushFont("MyriadProLightCond_16");
+                        Vector2 textSize = ImGui.CalcTextSize(windowId);
+                        Vector2 textPosition = DelvUI.Helpers.Utils.GetAnchoredPosition(elementPosition + elementSize / 2, textSize, Enums.DrawAnchor.Center);
+                        DelvUI.Helpers.DrawHelper.DrawShadowText(windowId, textPosition, ImGui.ColorConvertFloat4ToU32(new Vector4(1, 1, 1, Animator.Data.Opacity)), ImGui.ColorConvertFloat4ToU32(new Vector4(0, 0, 0, Animator.Data.Opacity)), drawList, 1);
+                        if (fontPushed) { ImGui.PopFont(); }
+                    }
+
+                    // Status Duration
+                    if (!InvertCheck)
+                    {
+                        float duration = status?.RemainingTime ?? 0;
+                        if (duration <= 0 && status != null && MaxDuration != null && Animator.TimeElapsed < 3000)
+                        {
+                            // Guess the duration until it is available in Dalamud?
+                            // Status duration seems to be 1 second longer than it should be?
+                            duration = (float)MaxDuration + 1 - (float)Animator.TimeElapsed / 1000;
                         }
 
-                        if (FlipImageVertically)
+                        if (duration > 0)
                         {
-                            uv0.Y = 1;
-                            uv1.Y = 0;
+                            string textDuration = duration.ToString("0.00", Plugin.NumberFormatInfo);
+                            bool fontPushed = DelvUI.Helpers.FontsManager.Instance.PushFont("MyriadProLightCond_20");
+                            Vector2 textSize = ImGui.CalcTextSize(textDuration);
+                            Vector2 textPosition = DelvUI.Helpers.Utils.GetAnchoredPosition(elementPosition + elementSize / 2, textSize, Enums.DrawAnchor.Center);
+                            DelvUI.Helpers.DrawHelper.DrawOutlinedText(textDuration, textPosition, ImGui.ColorConvertFloat4ToU32(new Vector4(1, 1, 1, 1)), ImGui.ColorConvertFloat4ToU32(new Vector4(0, 0, 0, 1)), drawList, 1);
+                            if (fontPushed) { ImGui.PopFont(); }
                         }
-
-                        drawList.AddImage(_texture.ImGuiHandle, elementPosition, elementPosition + elementSize, uv0, uv1, ImGui.ColorConvertFloat4ToU32(new Vector4(1, 1, 1, Animator.Data.Opacity)));
-					}
-					else
-					{
-						// Failover Text
-						drawList.AddRectFilled(elementPosition, elementPosition + elementSize, ImGui.ColorConvertFloat4ToU32(new Vector4(0, 0, 0, 0.5f * Animator.Data.Opacity)), 0);
-						drawList.AddRect(elementPosition, elementPosition + elementSize, ImGui.ColorConvertFloat4ToU32(new Vector4(1, 1, 1, 0.3f * Animator.Data.Opacity)), 0, ImDrawFlags.None, 1);
-
-						bool fontPushed = DelvUI.Helpers.FontsManager.Instance.PushFont("MyriadProLightCond_16");
-						Vector2 textSize = ImGui.CalcTextSize(windowId);
-						Vector2 textPosition = DelvUI.Helpers.Utils.GetAnchoredPosition(elementPosition + elementSize / 2, textSize, Enums.DrawAnchor.Center);
-						DelvUI.Helpers.DrawHelper.DrawShadowText(windowId, textPosition, ImGui.ColorConvertFloat4ToU32(new Vector4(1, 1, 1, Animator.Data.Opacity)), ImGui.ColorConvertFloat4ToU32(new Vector4(0, 0, 0, Animator.Data.Opacity)), drawList, 1);
-						if (fontPushed) { ImGui.PopFont(); }
-					}
-
-					// Status Duration
-					float duration = status?.RemainingTime ?? 0;
-					if (duration <= 0 && status != null && MaxDuration != null && Animator.TimeElapsed < 3000)
-					{
-						// Guess the duration until it is available in Dalamud?
-						// Status duration seems to be 1 second longer than it should be?
-						duration = (float)MaxDuration + 1 - (float)Animator.TimeElapsed / 1000;
-					}
-
-					if (duration > 0)
-					{
-						string textDuration = duration.ToString("0.00", Plugin.NumberFormatInfo);
-						bool fontPushed = DelvUI.Helpers.FontsManager.Instance.PushFont("MyriadProLightCond_20");
-						Vector2 textSize = ImGui.CalcTextSize(textDuration);
-						Vector2 textPosition = DelvUI.Helpers.Utils.GetAnchoredPosition(elementPosition + elementSize / 2, textSize, Enums.DrawAnchor.Center);
-						DelvUI.Helpers.DrawHelper.DrawOutlinedText(textDuration, textPosition, ImGui.ColorConvertFloat4ToU32(new Vector4(1, 1, 1, 1)), ImGui.ColorConvertFloat4ToU32(new Vector4(0, 0, 0, 1)), drawList, 1);
-						if (fontPushed) { ImGui.PopFont(); }
-					}
-				});
-			}
-		}
-	}
+                    }
+                });
+            }
+        }
+    }
 }
