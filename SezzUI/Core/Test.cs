ac9ff43d9adb932a52ab6f2e8f49163d413a186d
Martin Karer <sezz@sezz.at>
2022-01-15 20:30:53 +0100

    nothing

--- SezzUI/Core/Test.cs
+++ SezzUI/Core/Test.cs
@@ -11,16 +11,23 @@ using FFXIVClientStructs.FFXIV.Client.Game.UI;
 using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Client.UI.Misc;
 using FFXIVClientStructs.FFXIV.Client.Game.Group;
+using FFXIVClientStructs.FFXIV.Client.System.Framework;
+using System.Reflection;
 using System.Runtime.InteropServices;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using System.Diagnostics;
+using SezzUI.GameStructs;
 
 namespace SezzUI.Core
 {
-    public static class Test
+    public static unsafe class Test
     {
+
         public static unsafe void RunTest()
         {
             try
             {
+                Plugin.ChatGui.Print("Okay.");
             }
             catch (Exception ex)
             {
