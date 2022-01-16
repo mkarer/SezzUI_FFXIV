b64642b172bf3bb89a4fa28cbad2e9069dae3816
Martin Karer <sezz@sezz.at>
2022-01-16 13:29:43 +0100

    updated cooldowns

--- SezzUI/Modules/CooldownHud/Jobs/DRG.cs
+++ SezzUI/Modules/CooldownHud/Jobs/DRG.cs
@@ -7,6 +7,11 @@ namespace SezzUI.Modules.CooldownHud.Jobs
         public override void Configure(CooldownHud hud)
         {
             base.Configure(hud);
+
+            hud.RegisterCooldown(94); // Elusive Jump
+            hud.RegisterCooldown(95); // Spineshatter Dive
+            hud.RegisterCooldown(92); // Jump
+            hud.RegisterCooldown(16480); // Stardriver
         }
     }
 }
