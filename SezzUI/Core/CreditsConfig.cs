68c0d06b48e254524b41b4e1e022f43337a32f31
Martin Karer <sezz@sezz.at>
2022-01-21 23:40:29 +0100

    refactor

--- SezzUI/Core/CreditsConfig.cs
+++ SezzUI/Core/CreditsConfig.cs
@@ -1,40 +1,44 @@
-﻿using SezzUI.Config;
-using SezzUI.Config.Attributes;
+﻿using System.Numerics;
+using DelvUI.Helpers;
 using ImGuiNET;
-using System.Numerics;
 using Newtonsoft.Json;
+using SezzUI.Config;
+using SezzUI.Config.Attributes;
 
 namespace SezzUI.Interface.GeneralElements
 {
-    [Disableable(false)]
-    [Exportable(false)]
-    [Shareable(false)]
-    [Resettable(false)]
-    [Section("Credits")]
-    [SubSection("Credits", 0)]
-    public class CreditsConfig : PluginConfigObject
-    {
-        public new static CreditsConfig DefaultConfig() => new();
-        [JsonIgnore] private static Vector4 titleColor = new(0f / 255f, 174f / 255f, 255f / 255f, 1f);
+	[Disableable(false)]
+	[Exportable(false)]
+	[Shareable(false)]
+	[Resettable(false)]
+	[Section("Credits")]
+	[SubSection("Credits", 0)]
+	public class CreditsConfig : PluginConfigObject
+	{
+		public new static CreditsConfig DefaultConfig() => new();
 
-        [ManualDraw]
-        public bool Draw(ref bool changed)
-        {
-            ImGui.TextColored(titleColor, "DelvUI");
-            ImGui.Text("License: GNU AGPL v3");
-            ImGui.Text("Website:");
-            ImGui.SameLine();
-            if (ImGui.Button("https://github.com/DelvUI/DelvUI", new Vector2(0, 0)))
-            {
-                DelvUI.Helpers.Utils.OpenUrl("https://github.com/DelvUI/DelvUI");
-            }
-            ImGui.Text("Most of the base framework and configuration code is taken from DelvUI which saved me a tremendous amount of time. Big shoutout!");
-            ImGui.NewLine();
+		[JsonIgnore]
+		private static readonly Vector4 _titleColor = new(0f / 255f, 174f / 255f, 255f / 255f, 1f);
 
-            ImGui.TextColored(titleColor, "Additional credits:");
-            ImGui.Text("CaiClone, Lichie, professorK");
+		[ManualDraw]
+		public bool Draw(ref bool changed)
+		{
+			ImGui.TextColored(_titleColor, "DelvUI");
+			ImGui.Text("License: GNU AGPL v3");
+			ImGui.Text("Website:");
+			ImGui.SameLine();
+			if (ImGui.Button("https://github.com/DelvUI/DelvUI", new(0, 0)))
+			{
+				Utils.OpenUrl("https://github.com/DelvUI/DelvUI");
+			}
 
-            return false;
-        }
-    }
+			ImGui.Text("Most of the base framework and configuration code is taken from DelvUI which saved me a tremendous amount of time. Big shoutout!");
+			ImGui.NewLine();
+
+			ImGui.TextColored(_titleColor, "Additional credits:");
+			ImGui.Text("CaiClone, Lichie, professorK");
+
+			return false;
+		}
+	}
 }
