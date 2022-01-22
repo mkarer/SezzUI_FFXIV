00dbcc78df1e6f96130fe12920502a39ad59789a
Martin Karer <sezz@sezz.at>
2022-01-22 11:32:16 +0100

    refactor

--- SezzUI/Core/Events/Combat.cs
+++ SezzUI/Core/Events/Combat.cs
@@ -5,113 +5,119 @@ using Dalamud.Game.ClientState.Objects.Enums;
 
 namespace SezzUI.GameEvents
 {
-    internal sealed unsafe class Combat : BaseGameEvent
-    {
-        public event EventHandler? EnteringCombat;
-        public event EventHandler? LeavingCombat;
+	internal sealed class Combat : BaseGameEvent
+	{
+		public event EventHandler? EnteringCombat;
+		public event EventHandler? LeavingCombat;
 
-        private static readonly Lazy<Combat> ev = new Lazy<Combat>(() => new Combat());
-        public static Combat Instance { get { return ev.Value; } }
-        public static bool Initialized { get { return ev.IsValueCreated; } }
+		private bool _lastState;
 
-        private bool lastState = false;
+		#region Singleton
 
-        public override bool Enable()
-        {
-            if (base.Enable())
-            {
-                Plugin.Framework.Update += OnFrameworkUpdate;
-                return true;
-            }
-            
-            return false;
-        }
+		private static readonly Lazy<Combat> _ev = new(() => new());
+		public static Combat Instance => _ev.Value;
+		public static bool Initialized => _ev.IsValueCreated;
 
-        public override bool Disable()
-        {
-            if (base.Disable())
-            {
-                Plugin.Framework.Update -= OnFrameworkUpdate;
-                return true;
-            }
+		#endregion
 
-            return false;
-        }
+		public override bool Enable()
+		{
+			if (base.Enable())
+			{
+				_lastState = !IsInCombat();
+				Plugin.Framework.Update += OnFrameworkUpdate;
+				return true;
+			}
 
-        private void OnFrameworkUpdate(Framework framework)
-        {
-            try
-            {
-                Update();
-            }
-            catch (Exception ex)
-            {
-                LogError(ex, "OnFrameworkUpdate", $"Error: {ex}");
-            }
-        }
+			return false;
+		}
 
-        public bool IsInCombat(bool treatWeaponOutAsCombat = true)
-        {
-            bool state = lastState;
-            try
-            {
-                state = Plugin.Condition[ConditionFlag.InCombat] || (treatWeaponOutAsCombat && Plugin.ClientState.LocalPlayer != null && Plugin.ClientState.LocalPlayer.StatusFlags.HasFlag(StatusFlags.WeaponOut));
-            }
-            catch (Exception ex)
-            {
-                LogError(ex, "IsInCombat", $"Error: {ex}");
-            }
+		public override bool Disable()
+		{
+			if (base.Disable())
+			{
+				Plugin.Framework.Update -= OnFrameworkUpdate;
+				return true;
+			}
 
-            return state;
-        }
+			return false;
+		}
 
-        private void Update()
-        {
-            try
-            {
-                bool state = IsInCombat();
-                if (state != lastState)
-                {
-                    lastState = state;
-                    if (state)
-                    {
+		private void OnFrameworkUpdate(Framework framework)
+		{
+			try
+			{
+				Update();
+			}
+			catch (Exception ex)
+			{
+				LogError(ex, "OnFrameworkUpdate", $"Error: {ex}");
+			}
+		}
+
+		public bool IsInCombat(bool treatWeaponOutAsCombat = true)
+		{
+			bool state = _lastState;
+			try
+			{
+				state = Plugin.Condition[ConditionFlag.InCombat] || treatWeaponOutAsCombat && Plugin.ClientState.LocalPlayer != null && Plugin.ClientState.LocalPlayer.StatusFlags.HasFlag(StatusFlags.WeaponOut);
+			}
+			catch (Exception ex)
+			{
+				LogError(ex, "IsInCombat", $"Error: {ex}");
+			}
+
+			return state;
+		}
+
+		private void Update()
+		{
+			try
+			{
+				bool state = IsInCombat();
+				if (state != _lastState)
+				{
+					_lastState = state;
+					if (state)
+					{
 #if DEBUG
-                        if (EventManager.Config.LogEvents && EventManager.Config.LogEventCombatEnteringCombat)
-                        {
-                            LogDebug("EnteringCombat");
-                        }
+						if (EventManager.Config.LogEvents && EventManager.Config.LogEventCombatEnteringCombat)
+						{
+							LogDebug("EnteringCombat");
+						}
 #endif
-                        try
-                        {
-                            EnteringCombat?.Invoke(this, EventArgs.Empty);
-                        }
-                        catch (Exception ex)
-                        {
-                            LogError(ex, "EnteringCombat", $"Failed invoking {nameof(EnteringCombat)}: {ex}");
-                        }
-                    } else
-                    {
+						try
+						{
+							EnteringCombat?.Invoke(this, EventArgs.Empty);
+						}
+						catch (Exception ex)
+						{
+							LogError(ex, "EnteringCombat", $"Failed invoking {nameof(EnteringCombat)}: {ex}");
+						}
+					}
+					else
+					{
 #if DEBUG
-                        if (EventManager.Config.LogEvents && EventManager.Config.LogEventCombatLeavingCombat)
-                        {
-                            LogDebug("LeavingCombat");
-                        }
+						if (EventManager.Config.LogEvents && EventManager.Config.LogEventCombatLeavingCombat)
+						{
+							LogDebug("LeavingCombat");
+						}
 #endif
-                        try
-                        {
-                            LeavingCombat?.Invoke(this, EventArgs.Empty);
-                        }
-                        catch (Exception ex)
-                        {
-                            LogError(ex, "LeavingCombat", $"Failed invoking {nameof(LeavingCombat)}: {ex}");
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                LogError(ex, "Update", $"Error: {ex}");
-            }
-        }
-    }
+						try
+						{
+							LeavingCombat?.Invoke(this, EventArgs.Empty);
+						}
+						catch (Exception ex)
+						{
+							LogError(ex, "LeavingCombat", $"Failed invoking {nameof(LeavingCombat)}: {ex}");
+						}
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				LogError(ex, "Update", $"Error: {ex}");
+			}
+		}
+	}
 }
