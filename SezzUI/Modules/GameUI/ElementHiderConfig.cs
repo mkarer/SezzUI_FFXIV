1fbfd18a88c73c7828ade1b5dc589768e68c12b7
Martin Karer <sezz@sezz.at>
2022-01-28 21:23:49 +0100

    added areas to config

--- SezzUI/Modules/GameUI/ElementHiderConfig.cs
+++ SezzUI/Modules/GameUI/ElementHiderConfig.cs
@@ -1,4 +1,6 @@
-﻿using SezzUI.Config;
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using SezzUI.Config;
 using SezzUI.Config.Attributes;
 
 namespace SezzUI.Interface.GeneralElements
@@ -10,11 +12,50 @@ namespace SezzUI.Interface.GeneralElements
 		[Checkbox("Hide ActionBar Lock", isMonitored = true)]
 		[Order(1)]
 		public bool HideActionBarLock;
+		
+		[NestedConfig("Area 1", 21)]
+		public InteractableAreaConfig Area1 = new();
+
+		[NestedConfig("Area 2", 22)]
+		public InteractableAreaConfig Area2 = new();
+
+		[NestedConfig("Area 3", 23)]
+		public InteractableAreaConfig Area3 = new();
+
+		[NestedConfig("Area 4", 24)]
+		public InteractableAreaConfig Area4 = new();
+
+		[NestedConfig("Area 5", 25)]
+		public InteractableAreaConfig Area5 = new();
+
+		[NestedConfig("Area 6", 26)]
+		public InteractableAreaConfig Area6 = new();
+
+		[NestedConfig("Area 7", 27)]
+		public InteractableAreaConfig Area7 = new();
+
+		[NestedConfig("Area 8", 28)]
+		public InteractableAreaConfig Area8 = new();
+
+		[NestedConfig("Area 9", 29)]
+		public InteractableAreaConfig Area9 = new();
+
+		[NestedConfig("Area 10", 30)]
+		public InteractableAreaConfig Area10 = new();
+
+		[JsonIgnore]
+		public List<InteractableAreaConfig> Areas;
+
+		public ElementHiderConfig()
+		{
+			Areas = new() {Area1, Area2, Area3, Area4, Area5, Area6, Area7, Area8, Area9, Area10};
+		}
 
 		public ElementHiderConfig Reset()
 		{
 			Enabled = true;
 			HideActionBarLock = true;
+			Areas.ForEach(area => area.Reset());
 			return this;
 		}
 
--- SezzUI/Modules/GameUI/InteractableAreaConfig.cs
+++ SezzUI/Modules/GameUI/InteractableAreaConfig.cs
@@ -1,13 +1,47 @@
-﻿using SezzUI.Config;
+﻿using System.Collections.Generic;
+using System.Numerics;
+using SezzUI.Config;
+using SezzUI.Config.Attributes;
+using SezzUI.Enums;
 
 namespace SezzUI.Interface.GeneralElements
 {
 	public class InteractableAreaConfig : AnchorablePluginConfigObject
 	{
-		public new static InteractableAreaConfig DefaultConfig() =>
-			new()
-			{
-				Enabled = true
-			};
+		[InputText("Description (Optional)", formattable = false)]
+		[Order(0)]
+		public string Description = "";
+		
+		[MultiSelectorAttribute("Elements")]
+		[IntStringPair((int)Addon.ActionBar1, "Action Bar 1")]
+		[IntStringPair((int)Addon.ActionBar2, "Action Bar 2")]
+		[IntStringPair((int)Addon.ActionBar3, "Action Bar 3")]
+		[IntStringPair((int)Addon.ActionBar4, "Action Bar 4")]
+		[IntStringPair((int)Addon.ActionBar5, "Action Bar 5")]
+		[IntStringPair((int)Addon.ActionBar6, "Action Bar 6")]
+		[IntStringPair((int)Addon.ActionBar7, "Action Bar 7")]
+		[IntStringPair((int)Addon.ActionBar8, "Action Bar 8")]
+		[IntStringPair((int)Addon.ActionBar9, "Action Bar 9")]
+		[IntStringPair((int)Addon.ActionBar10, "Action Bar 10")]
+		[IntStringPair((int)Addon.MainMenu, "Main Menu")]
+		[IntStringPair((int)Addon.ScenarioGuide, "Scenario Guide")]
+		[Order(20)]
+		// ReSharper disable once CollectionNeverUpdated.Global
+		public List<int> Elements = new();
+		
+		// TODO: DrawState/ParentAddon condition?
+		
+		public InteractableAreaConfig Reset()
+		{
+			Enabled = false;
+			Description = "";
+			Position = Vector2.Zero;
+			Size = new(300, 100);
+			Anchor = DrawAnchor.Center;
+			Elements.Clear();
+			return this;
+		}
+
+		public new static InteractableAreaConfig DefaultConfig() => new InteractableAreaConfig().Reset();
 	}
 }
