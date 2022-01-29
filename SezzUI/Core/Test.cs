185f3cbfb64339aac045c0145dc6470ab58530db
Martin Karer <sezz@sezz.at>
2022-01-29 15:07:13 +0100

    testing addon visibility flags

--- SezzUI/Core/Test.cs
+++ SezzUI/Core/Test.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using FFXIVClientStructs.FFXIV.Component.GUI;
 using SezzUI.Helpers;
 
 namespace SezzUI.Core
@@ -8,15 +10,60 @@ namespace SezzUI.Core
 	{
 		internal static PluginLogger Logger = new("Test");
 
+		private static string ToFixedLength(string input, int length)
+		{
+			if (input.Length > length)
+				return input.Substring(0, length);
+			else
+				return input.PadRight(length, ' ');
+		}
+
+		private static string ToBinaryString(byte number)
+		{
+			return Convert.ToString(number, 2).PadLeft(8, '0');
+		}
+
 		public static void RunTest()
 		{
 			try
 			{
 				Plugin.ChatGui.Print("Okay.");
-				Plugin.ChatGui.Print(SpellHelper.GetAdjustedActionId(25800u, true).ToString());
-				Plugin.ChatGui.Print(SpellHelper.GetAdjustedActionId(7427u, true).ToString());
-				Plugin.ChatGui.Print(SpellHelper.GetAdjustedActionId(7428u, true).ToString());
-				Plugin.ChatGui.Print(SpellHelper.GetAdjustedActionId(7429u, true).ToString());
+
+				AtkStage* stage = AtkStage.GetSingleton();
+				if (stage == null)
+				{
+					return;
+				}
+
+				AtkUnitList* loadedUnitsList = &stage->RaptureAtkUnitManager->AtkUnitManager.AllLoadedUnitsList;
+				if (loadedUnitsList == null)
+				{
+					return;
+				}
+
+				AtkUnitBase** addonList = &loadedUnitsList->AtkUnitEntries;
+				if (addonList == null)
+				{
+					return;
+				}
+
+				for (int i = 0; i < loadedUnitsList->Count; i++)
+				{
+					AtkUnitBase* addon = addonList[i];
+					if (addon == null || addon->RootNode == null || addon->UldManager.LoadedState != 3)
+					{
+						continue;
+					}
+
+					string? name = Marshal.PtrToStringAnsi(new(addon->Name));
+					if (name == null)
+					{
+						continue;
+					}
+
+					byte visibilityFlag = *((byte*) addon + 0x1B6);
+					Logger.Debug("RunTest", $"Addon: {ToFixedLength(name, 24)} | Visibility: {visibilityFlag} {ToBinaryString(visibilityFlag)}");
+				}
 			}
 			catch (Exception ex)
 			{
