d41ea44454d56130ee5c9941169416d7d179bc6b
Martin Karer <sezz@sezz.at>
2022-02-02 14:50:18 +0100

    fix: path validation feat: file validation

--- SezzUI/Core/Helpers/FileSystemHelper.cs
+++ SezzUI/Core/Helpers/FileSystemHelper.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using Dalamud.Utility;
 
 namespace SezzUI.Helpers
 {
@@ -12,22 +13,54 @@ namespace SezzUI.Helpers
 			Logger = new("FileSystemHelper");
 		}
 
-		public static string ValidatePath(string path)
+		public static bool ValidatePath(string? path, out string validatedPath)
 		{
+			validatedPath = "";
+			if (path.IsNullOrEmpty())
+			{
+				return false;
+			}
+
 			try
 			{
-				string fullPath = Path.GetFullPath(path);
+				string fullPath = Path.GetFullPath(path!);
 				if (fullPath != "" && Directory.Exists(fullPath))
 				{
-					return fullPath.TrimEnd(Path.DirectorySeparatorChar) + Path.DirectorySeparatorChar;
+					validatedPath = fullPath.TrimEnd(Path.DirectorySeparatorChar) + Path.DirectorySeparatorChar;
+					return true;
 				}
 			}
 			catch (Exception ex)
 			{
-				Logger.Error(ex, "ValidatePath", "Error: {ex}");
+				Logger.Warning(ex, "ValidatePath", $"Failed validating path: {path} Error: {ex}");
 			}
 
-			return "";
+			return false;
+		}
+
+		public static bool ValidateFile(string? path, string? file, out string validatedFileName)
+		{
+			validatedFileName = "";
+			if (!ValidatePath(path, out string validatedPath) || file.IsNullOrEmpty())
+			{
+				return false;
+			}
+
+			try
+			{
+				string filePath = validatedPath + file!.TrimStart(Path.DirectorySeparatorChar);
+				if (File.Exists(filePath) && !File.GetAttributes(filePath).HasFlag(FileAttributes.Directory))
+				{
+					validatedFileName = filePath;
+					return true;
+				}
+			}
+			catch (Exception ex)
+			{
+				Logger.Warning(ex, "ValidateFile", $"Failed validating file: {file} Error: {ex}");
+			}
+
+			return false;
 		}
 	}
 }
