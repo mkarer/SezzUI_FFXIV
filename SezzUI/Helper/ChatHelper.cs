28ae962c0fe8244f2c0d2ea6f5ffd25a29215831
Martin Karer <sezz@sezz.at>
2025-10-08 02:51:43 +0200

    fix(chathelper): updated + simplified

--- SezzUI/Helper/ChatHelper.cs
+++ SezzUI/Helper/ChatHelper.cs
@@ -2,89 +2,23 @@
 // https://github.com/Caraxi/SimpleTweaksPlugin/blob/main/Utility/ChatHelper.cs
 
 using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Runtime.InteropServices;
-using System.Text;
-using FFXIVClientStructs.FFXIV.Client.System.Memory;
 using FFXIVClientStructs.FFXIV.Client.System.String;
-using Framework = FFXIVClientStructs.FFXIV.Client.System.Framework.Framework;
+using FFXIVClientStructs.FFXIV.Client.UI;
 
 namespace SezzUI.Helper;
 
 /// <summary>
 ///     A class containing chat functionality
 /// </summary>
-[SuppressMessage("ReSharper", "InconsistentNaming")]
-[SuppressMessage("ReSharper", "SuggestVarOrType_BuiltInTypes")]
 public static class ChatHelper
 {
-	private static class Signatures
-	{
-		internal const string SendChat = "48 89 5C 24 ?? 48 89 74 24 ?? 57 48 83 EC ?? 48 8B F2 48 8B F9 45 84 C9";
-		internal const string SanitiseString = "E8 ?? ?? ?? ?? 48 8D 4C 24 ?? 0F B6 F0 E8 ?? ?? ?? ?? 48 8D 4D C0";
-	}
-
-	private delegate void ProcessChatBoxDelegate(IntPtr uiModule, IntPtr message, IntPtr unused, byte a4);
-
-	private static ProcessChatBoxDelegate? ProcessChatBox { get; }
-
-	private static readonly unsafe delegate* unmanaged<Utf8String*, int, IntPtr, void> _sanitiseString = null!;
-
-	static ChatHelper()
-	{
-		if (Services.SigScanner.TryScanText(Signatures.SendChat, out var processChatBoxPtr))
-		{
-			ProcessChatBox = Marshal.GetDelegateForFunctionPointer<ProcessChatBoxDelegate>(processChatBoxPtr);
-		}
-
-		unsafe
-		{
-			if (Services.SigScanner.TryScanText(Signatures.SanitiseString, out var sanitisePtr))
-			{
-				_sanitiseString = (delegate* unmanaged<Utf8String*, int, IntPtr, void>) sanitisePtr;
-			}
-		}
-	}
-
 	/// <summary>
 	///     <para>
 	///         Send a given message to the chat box. <b>This can send chat to the server.</b>
 	///     </para>
 	///     <para>
-	///         <b>This method is unsafe.</b> This method does no checking on your input and
-	///         may send content to the server that the normal client could not. You must
-	///         verify what you're sending and handle content and length to properly use
-	///         this.
-	///     </para>
-	/// </summary>
-	/// <param name="message">Message to send</param>
-	/// <exception cref="InvalidOperationException">If the signature for this function could not be found</exception>
-	public static unsafe void SendMessageUnsafe(byte[] message)
-	{
-		if (ProcessChatBox == null)
-		{
-			throw new InvalidOperationException("Could not find signature for chat sending");
-		}
-
-		var uiModule = (IntPtr) Framework.Instance()->GetUIModule();
-
-		using ChatPayload payload = new(message);
-		var mem1 = Marshal.AllocHGlobal(400);
-		Marshal.StructureToPtr(payload, mem1, false);
-
-		ProcessChatBox(uiModule, mem1, IntPtr.Zero, 0);
-
-		Marshal.FreeHGlobal(mem1);
-	}
-
-	/// <summary>
-	///     <para>
-	///         Send a given message to the chat box. <b>This can send chat to the server.</b>
-	///     </para>
-	///     <para>
-	///         This method is slightly less unsafe than <see cref="SendMessageUnsafe" />. It
-	///         will throw exceptions for certain inputs that the client can't normally send,
-	///         but it is still possible to make mistakes. Use with caution.
+	///         This method will throw exceptions for certain inputs that the client can't
+	///         normally send, but it is still possible to make mistakes. Use with caution.
 	///     </para>
 	/// </summary>
 	/// <param name="message">message to send</param>
@@ -92,90 +26,49 @@ public static class ChatHelper
 	///     If <paramref name="message" /> is empty, longer than 500 bytes in UTF-8, or
 	///     contains invalid characters.
 	/// </exception>
-	/// <exception cref="InvalidOperationException">If the signature for this function could not be found</exception>
-	public static void SendMessage(string message)
+	/// <exception cref="InvalidOperationException">
+	///     If the signature for this function could not be found -or- The UiModule is
+	///     currently unavailable
+	/// </exception>
+	public static unsafe void SendMessage(string message)
 	{
-		var bytes = Encoding.UTF8.GetBytes(message);
-		if (bytes.Length == 0)
+		Utf8String* utf8 = Utf8String.FromString(message);
+
+		try
 		{
-			throw new ArgumentException("message is empty", nameof(message));
+			if (utf8->Length == 0)
+			{
+				throw new ArgumentException("message is empty", nameof(message));
+			}
+
+			if (utf8->Length > 500)
+			{
+				throw new ArgumentException("message is longer than 500 bytes", nameof(message));
+			}
+
+			int oldLength = utf8->Length;
+
+			utf8->SanitizeString(AllowedEntities.UppercaseLetters | AllowedEntities.LowercaseLetters | AllowedEntities.Numbers | AllowedEntities.SpecialCharacters | AllowedEntities.CharacterList | AllowedEntities.OtherCharacters | AllowedEntities.Payloads | AllowedEntities.Unknown9);
+
+			if (utf8->Length != oldLength)
+			{
+				throw new ArgumentException("message contained invalid characters", nameof(message));
+			}
+
+			UIModule* uiModule = UIModule.Instance();
+			if (uiModule == null)
+			{
+				throw new InvalidOperationException("The UiModule is currently unavailable");
+			}
+
+			uiModule->ProcessChatBoxEntry(utf8);
 		}
-
-		if (bytes.Length > 500)
+		finally
 		{
-			throw new ArgumentException("message is longer than 500 bytes", nameof(message));
-		}
-
-		if (message.Length != SanitiseText(message).Length)
-		{
-			throw new ArgumentException("message contained invalid characters", nameof(message));
-		}
-
-		SendMessageUnsafe(bytes);
-	}
-
-	/// <summary>
-	///     <para>
-	///         Sanitises a string by removing any invalid input.
-	///     </para>
-	///     <para>
-	///         The result of this method is safe to use with
-	///         <see cref="SendMessage" />, provided that it is not empty or too
-	///         long.
-	///     </para>
-	/// </summary>
-	/// <param name="text">text to sanitise</param>
-	/// <returns>sanitised text</returns>
-	/// <exception cref="InvalidOperationException">If the signature for this function could not be found</exception>
-	public static unsafe string SanitiseText(string text)
-	{
-		if (_sanitiseString == null)
-		{
-			throw new InvalidOperationException("Could not find signature for chat sanitisation");
-		}
-
-		Utf8String* uText = Utf8String.FromString(text);
-
-		_sanitiseString(uText, 0x27F, IntPtr.Zero);
-		var sanitised = uText->ToString();
-
-		uText->Dtor();
-		IMemorySpace.Free(uText);
-
-		return sanitised;
-	}
-
-	[StructLayout(LayoutKind.Explicit)]
-	[SuppressMessage("ReSharper", "PrivateFieldCanBeConvertedToLocalVariable")]
-	private readonly struct ChatPayload : IDisposable
-	{
-		[FieldOffset(0)]
-		private readonly IntPtr textPtr;
-
-		[FieldOffset(16)]
-		private readonly ulong textLen;
-
-		[FieldOffset(8)]
-		private readonly ulong unk1;
-
-		[FieldOffset(24)]
-		private readonly ulong unk2;
-
-		internal ChatPayload(byte[] stringBytes)
-		{
-			textPtr = Marshal.AllocHGlobal(stringBytes.Length + 30);
-			Marshal.Copy(stringBytes, 0, textPtr, stringBytes.Length);
-			Marshal.WriteByte(textPtr + stringBytes.Length, 0);
-
-			textLen = (ulong) (stringBytes.Length + 1);
-
-			unk1 = 64;
-			unk2 = 0;
-		}
-
-		public void Dispose()
-		{
-			Marshal.FreeHGlobal(textPtr);
+			if (utf8 != null)
+			{
+				utf8->Dtor(true);
+			}
 		}
 	}
 }
